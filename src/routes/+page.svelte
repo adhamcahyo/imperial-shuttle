eb96c81e3d54196de88102fa571603498e44ce81
adhamcahyo <nebelwerfersf@gmail.com>
2023-07-23 23:05:22 +0700

    integrate tailwind ui based component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="m-2 p-2 flex flex-col bg-slate-500">
+    <h1 class="text-3xl font-bold underline text-white">
+        Hello world!
+    </h1>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
